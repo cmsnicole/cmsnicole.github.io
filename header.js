02f326f63cfe62aa3dc25846758b14ba4e2cb8b3
msc552 <nicolesumsum143388@gmail.com>
2024-02-10 23:29:22 +0000

    aaa

--- header.js
+++
@@ -1,6 +0,0 @@
-const dropdown = document.querySelector('.nav-small-dropdown')
-const menuButton = document.getElementById('header-menu');
-
-menuButton.addEventListener('click', () => {
-    dropdown.classList.toggle('active');
-})
